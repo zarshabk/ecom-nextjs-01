520cb6ac1406c26e7175c3ee1ea95fc6ecdd51ba
zarshab <zarshabrafiq1092@gmail.com>
2024-04-19 12:28:10 +0500

    change again

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <LayoutComp children={children} />
+        <LayoutComp>{children}</LayoutComp>
       </body>
     </html>
   );
