c2a11ebe5c90669bb5edce755e278c1547927a7b
zarshab <zarshabrafiq1092@gmail.com>
2024-04-19 15:20:58 +0500

    slve layout redux issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import LayoutComp from "@/components/LayoutComp";
+
+import Header from "@/components/Header";
+import { Footer } from "flowbite-react";
+import { store } from "@/redux/store";
+import Providers from "@/redux/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +17,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <LayoutComp>{children}</LayoutComp>
+        <Providers>
+          <Header />
+          <main className="container w-[98%] lg:w-[80%] md:w-[90%] m-auto">
+            {children}
+          </main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
